aacb832bbfe9fe6cdbad650ef5dd6f04f68398f0
PointSource <47544779+point-source@users.noreply.github.com>
2021-07-28 10:21:22 -0700

    Add rentalworksWithAPI method

--- lib/src/rentalworksWithAPI.dart
+++ lib/src/rentalworksWithAPI.dart
@@ -1,17 +1,65 @@
 import 'package:chopper/chopper.dart';
-import 'package:rentalworks/generated_code/rentalworks.swagger.dart';
+import 'package:rentalworks/generated_code/account_services.swagger.dart';
+import 'package:rentalworks/generated_code/exports.swagger.dart';
+import 'package:rentalworks/generated_code/plugins.swagger.dart';
+import 'package:rentalworks/src/json_serializable_converter.dart';
 
-RentalWorks rentalworksWithApiKey(String apiKey) {
-  final newClient = ChopperClient(
-      services: [
-        RentalWorks.create()
-      ],
-      interceptors: [
-        // Auth Interceptor
-        (Request request) async =>
-            applyHeader(request, 'LASSO-APIKEY', apiKey, override: false),
-      ],
-      converter: JsonSerializableConverter(),
-      baseUrl: 'https://fuse.rentalworks.io/api/v1');
-  return RentalWorks.create(newClient);
+class RentalWorks {
+  RentalWorks.withJWT(this.baseUrl, this._jwt);
+
+  RentalWorks.withCredentials(this.baseUrl, this._username, this._password);
+
+  String baseUrl;
+  String? _jwt;
+  String? _username;
+  String? _password;
+
+  Future<String> get jwt async =>
+      _jwt ??= await _fetchJWT(_username, _password);
+
+  Future<String> _fetchJWT(String? username, String? password) async {
+    if (_username == null) {
+      throw Exception('No username or token supplied');
+    }
+    if (_password == null) {
+      throw Exception('No password or token supplied');
+    }
+    final response = await accountServices.apiV1JwtPost(
+        body: FwStandardModelsFwApplicationUser(
+            userName: _username, password: _password));
+    if (response.isSuccessful) {
+      final jwt = response.body?.accessToken;
+      if (jwt != null) return jwt;
+    }
+    throw Exception(response.error);
+  }
+
+  Future<Request> Function(Request) get _jwtInterceptor =>
+      (Request request) async =>
+          applyHeader(request, 'Authorization', await jwt, override: false);
+
+  AccountServices? _accountServices;
+
+  AccountServices get accountServices =>
+      _accountServices ??= AccountServices.create(ChopperClient(
+          services: [AccountServices.create()],
+          converter:
+              JsonSerializableConverter(AccountServicesJsonDecoderMappings),
+          baseUrl: baseUrl));
+
+  Exports? _exports;
+
+  Exports get exports => _exports ??= Exports.create(ChopperClient(
+      services: [Exports.create()],
+      interceptors: [_jwtInterceptor],
+      converter: JsonSerializableConverter(ExportsJsonDecoderMappings),
+      baseUrl: baseUrl));
+
+  Plugins? _plugins;
+
+  Plugins get plugins => _plugins ??= Plugins.create(ChopperClient(
+      services: [Plugins.create()],
+      interceptors: [_jwtInterceptor],
+      converter: JsonSerializableConverter(PluginsJsonDecoderMappings),
+      baseUrl: baseUrl));
 }
